46b740fd8a181f51063e5213192252e1696fa959
lokn <lokn329@aliyun.com>
2021-10-24 20:26:19 +0800

    first commit

---
+++ src/main/java/com/furt/test/TestDemo.java
@@ -0,0 +1,14 @@
+package com.furt.test;
+
+/**
+ * @description:
+ * @author: lokn
+ * @date: 2021/10/24 20:22
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+
+}
